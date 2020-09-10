19fd565c154228cbe88827f99e8d53c35ee5c148
Emma Li <emma.m.li.98@gmail.com>
2020-09-09 20:06:10 -0700

    Sets up chat database, tables in pure SQL

--- server/schema.sql
+++ server/schema.sql
@@ -3,15 +3,25 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  message_text TEXT,
+  time_stamp TIMESTAMP,
+  user_id INT,
+  room_id INT
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  room_name TEXT
+);
 
-
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username TEXT
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
